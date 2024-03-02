a4455bb7cde14d03f522f5fceeb1f6cc7c40c0e7
swapnanilWebDeveloper <swapnanildev123@gmail.com>
2024-03-02 13:46:43 +0530

    combined reducers in redux

---
+++ combined reducers/index_1.js
@@ -0,0 +1,108 @@
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import axios from 'axios';
+
+// action name constants
+const inc = 'account/increment';
+const dec = 'account/decrement';
+const incByAmt = 'account/incrementByAmount';
+// const init = 'account/initilaize';
+const getAccUserPending = 'account/getUser/pending';
+const getAccUserFulfilled= 'account/getUser/fulfilled';
+const getAccUserRejected = 'account/getUser/rejected';
+
+const incBonus = 'bonus/increment';
+//store
+const store = createStore(
+    combineReducers({
+        account : accountReducer,
+        bonus : bonusReducer
+    }), 
+    applyMiddleware(logger.default, thunk.default));
+
+// reducer
+function accountReducer(state={amount : 1}, action){
+      switch(action.type){
+         case getAccUserFulfilled: 
+              return {amount : action.payload, pending : false};
+         case getAccUserRejected: 
+              return {...state, error : action.error, pending : false};
+         case getAccUserPending: 
+              return {...state, pending : true};
+        
+         case inc: 
+              return {amount : state.amount + 1};
+         case dec: 
+              return {amount : state.amount - 1};
+         case incByAmt: 
+              return {amount : state.amount + action.payload}; 
+         default :
+              return state;    
+      }
+}
+
+function bonusReducer(state={points : 0}, action){
+    switch(action.type){ 
+        case incBonus:
+             return {points : state.points + 1}
+        case incByAmt: 
+             if(action.payload >= 100)
+                 return {points : state.points + 1};
+        default :
+              return state; 
+    }
+}
+// Action creators
+function getUserAccount(id){
+        return async (dispatch,getState) => {
+            try{
+                dispatch(getAccountUserPending());
+                const {data} = await axios.get(`http://localhost:3000/accounts/${id}`);
+                dispatch(getAccountUserFulFilled(data.amount))
+            }
+            catch(error){
+               // console.log("error fetching data....."+error);
+                dispatch(getAccountUserRejected(error.message));
+            }
+        }
+}
+
+function getAccountUserFulFilled(value){
+     return {type : getAccUserFulfilled, payload : value}
+}
+
+function getAccountUserRejected(error){
+    return {type : getAccUserRejected, error : error}
+}
+
+function getAccountUserPending(){
+    return {type : getAccUserPending }
+}
+
+function increment(){
+    return {type : inc }
+}
+
+function decrement(){
+    return {type : dec}
+}
+
+function incrementByAmount(value){
+    return {type : incByAmt, payload : value}
+}
+
+function incrementBonus(){
+    return {type : incBonus}
+}
+
+/* setInterval(() => {
+    store.dispatch(incrementByAmount(5));
+},2000) */
+
+setTimeout(() => {
+    store.dispatch(getUserAccount(2));
+ //   store.dispatch(increment());
+ //   store.dispatch(incrementByAmount(150));
+ //    store.dispatch(incrementBonus());
+},2000)
